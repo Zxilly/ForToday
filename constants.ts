6086924f62a0e17618c7b9b6cd8bb9117b81938c
Zxilly <zxilly@outlook.com>
2022-11-03 22:06:19 +0800

    feat: group

---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const targets = ["GlenBzc", "Algor_", "hirsute", "suidingyunmeinv"]
+
+export const trainings = [
+    "407778"
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 // noinspection JSIgnoredPromiseFromCall
 
 import {Box, Container, SimpleGrid} from '@chakra-ui/react'
+import {targets, trainings} from "../constants";
+import {JSDOM} from "jsdom";
 
 interface Result {
     name: string,
@@ -8,7 +10,6 @@ interface Result {
     failedProblems: string[],
 }
 
-const targets = ["GlenBzc", "Algor_", "hirsute", "suidingyunmeinv"]
 
 export default function Home({results}: { results: Result[] }) {
     return (
@@ -62,12 +63,77 @@ export default function Home({results}: { results: Result[] }) {
 }
 
 export async function getServerSideProps() {
+    const submitSuccess = new Map<string, Set<string>>();
+    const submitMap = new Map<string, Set<string>>();
+    const submitFailedMap = new Map<string, Set<string>>();
+
+    for (const target of targets) {
+        submitSuccess.set(target, new Set<string>());
+        submitMap.set(target, new Set<string>());
+        submitFailedMap.set(target, new Set<string>());
+    }
+    for (const trainID of trainings) {
+        const url = `https://codeforces.com/group/MlnUj8Knxs/contest/${trainID}/status`
+        const response = await fetch(url).then((res) => res.text())
+
+        const doc  = new JSDOM(response).window.document
+        const table = doc.querySelector('table.status-frame-datatable')
+        if (table == null) {
+            continue
+        }
+        // not have class first-row
+        const rows = table.querySelectorAll('tr:not(.first-row)')
+        for (const row of Array.from(rows)) {
+            const cells = row.querySelectorAll('td')
+            const time = row.querySelector('span.format-time')
+            if (!time) {
+                continue
+            }
+            const timeStr = time.textContent
+            if (!timeStr) {
+                continue
+            }
+            const date = new Date(timeStr)
+            if (date.getDate() != new Date().getDate()) {
+                continue
+            }
+
+            const submitUser = cells[2].textContent?.trim().replaceAll('\n', '')
+            if (!submitUser) {
+                continue
+            }
+            if (!targets.includes(submitUser)) {
+                continue
+            }
+
+            const problemID = cells[3].textContent
+            if (!problemID) {
+                continue
+            }
+            const status = row.querySelector('span.verdict-accepted')
+            if (status) {
+                submitSuccess.get(submitUser)?.add(problemID)
+            } else {
+                submitMap.get(submitUser)?.add(problemID)
+            }
+        }
+
+        for (const [user, problems] of Array.from(submitMap)) {
+            for (const problem of Array.from(problems)) {
+                if (!submitSuccess.get(user)?.has(problem)) {
+                    submitFailedMap.get(user)?.add(problem)
+                }
+            }
+        }
+    }
+
+
     let results: Array<Result> = [];
     for (const member of targets) {
         const res = await fetch(`https://codeforces.com/api/user.status?handle=${member}&from=1&count=50`)
             .then(res => res.json())
-        const passProblems = new Set()
-        const problems = new Set()
+        const passProblems = new Set<string>()
+        const problems = new Set<string>()
 
         for (const sub of res["result"]) {
             const timeSecs = sub["creationTimeSeconds"]
@@ -85,11 +151,15 @@ export async function getServerSideProps() {
         }
 
         const failedProblems = Array.from(problems).filter(x => !passProblems.has(x))
+
+        passProblems.forEach(x => submitSuccess.get(member)?.add(x))
+        failedProblems.forEach(x => submitFailedMap.get(member)?.add(x))
+
         results.push({
             name: member,
-            successProblems: Array.from(passProblems) as string[],
-            failedProblems: failedProblems as string[],
-        });
+            successProblems: Array.from(submitSuccess.get(member) ?? []),
+            failedProblems: Array.from(submitFailedMap.get(member) ?? []),
+        })
     }
 
     return {
