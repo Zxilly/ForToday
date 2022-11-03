5e7175955b17b2b8c91f4337ccd069b6cb1b24a6
Zxilly <zxilly@outlook.com>
2022-11-03 22:20:46 +0800

    chore: reduce

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const targets = ["GlenBzc", "Algor_", "hirsute", "suidingyunmeinv", "Vionia", "YuNostalgia", "zxilly", "AgOH"]
+export const targets = ["GlenBzc", "Algor_", "hirsute", "suidingyunmeinv", "Vionia", "YuNostalgia", "AgOH"]
 
 export const trainings = [
     "407778"
