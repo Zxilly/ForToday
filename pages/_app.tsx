575522b9761ffed40459dbbf2fc787a3d21be949
Zxilly <zhouxinyu1001@gmail.com>
2022-11-05 00:53:35 +0800

    feat: add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type {AppProps} from 'next/app'
 import {ChakraProvider} from '@chakra-ui/react'
 import {useEffect} from "react";
 import {Analytics} from '@vercel/analytics/react';
+import Head from "next/head";
 
 
 export default function App({Component, pageProps}: AppProps) {
@@ -13,6 +14,9 @@ export default function App({Component, pageProps}: AppProps) {
     }, [])
 
     return <>
+        <Head>
+            <title>Three days</title>
+        </Head>
         <ChakraProvider>
             <Component {...pageProps} />
         </ChakraProvider>
