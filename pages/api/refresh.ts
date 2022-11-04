86735069e2f652a4cd9083d604e98d08a94f2041
Zxilly <zhouxinyu1001@gmail.com>
2022-11-05 03:05:01 +0800

    fix: add lock log

--- pages/api/refresh.ts
+++ pages/api/refresh.ts
@@ -7,18 +7,25 @@ export default async function handler(
     request: NextApiRequest,
     response: NextApiResponse,
 ) {
+    const logger = getNewTimedLogger()
+    logger("Adding lock...");
     const lock = await client.setnx("lock", "1");
     if (!lock) {
         response.status(200).json({message: "locked"});
         return;
     }
     await client.expire("lock", 15);
+    logger("Lock added.");
 
-    const logger = getNewTimedLogger()
-
+    logger("Fetching...")
     const data = await fetchAll(logger)
+    logger("Fetched.")
+    logger("Saving...")
     await client.set("data", JSON.stringify(data))
+    logger("Saved.")
+    logger("Removing lock...")
     await client.del("lock")
+    logger("Lock removed.")
     response.status(200).json({
         time: new Date().getTime(),
     });
