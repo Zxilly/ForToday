227b3c4d2aaf51f84f7a048b4b669cf9cd06102e
Zxilly <zxilly@outlook.com>
2023-11-12 00:05:22 +0800

    fix: fix empty page

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,6 +1,10 @@
 import { Box, Link } from "@chakra-ui/react";
 import { groupBy, rankColor } from "../utils/utils";
-import { ProblemWithStatus, PureUserProblemStatus, UserProblemStatus } from "../types/tentacle";
+import {
+	ProblemWithStatus,
+	PureUserProblemStatus,
+	UserProblemStatus,
+} from "../types/tentacle";
 import React, { useEffect } from "react";
 import Lottie from "lottie-react";
 import animationA from "../animation/A.json";
@@ -8,153 +12,158 @@ import animationB from "../animation/B.json";
 import animationC from "../animation/C.json";
 
 export type UserCardProps = {
-    name: string;
-    status: PureUserProblemStatus;
+	name: string;
+	status: PureUserProblemStatus;
 };
 
-export function UserCard(props: UserCardProps): JSX.Element
-{
-    const { name, status } = props;
-    const color = status.rank !== undefined && status.rank >= 0 ? rankColor(status.rank) : "black";
+export function UserCard(props: UserCardProps): JSX.Element {
+	const { name, status } = props;
+	const color =
+		status.rank !== undefined && status.rank >= 0
+			? rankColor(status.rank)
+			: "black";
 
+	const [animation, setAnimation] = React.useState<any>(animationA);
+	useEffect(() => {
+		switch ((status.rank + 1) % 3) {
+			case 0:
+				setAnimation(animationA);
+				break;
+			case 1:
+				setAnimation(animationB);
+				break;
+			case 2:
+				setAnimation(animationC);
+				break;
+		}
+	}, [status.rank]);
 
-    const [animation, setAnimation] = React.useState<any>(animationA);
-    useEffect(() =>
-    {
-        switch ((status.rank + 1) % 3)
-        {
-            case 0:
-                setAnimation(animationA);
-                break;
-            case 1:
-                setAnimation(animationB);
-                break;
-            case 2:
-                setAnimation(animationC);
-                break;
-        }
-    }, [status.rank]);
-
-    return (
-        <Box
-            padding={4}
-            key={name}
-            borderWidth="3px"
-            borderRadius="lg"
-            borderColor={color}
-            overflow="hidden"
-            style={{
-                background: `linear-gradient(250deg, ${color} 2rem, var(--chakra-colors-chakra-body-bg) 0)`,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "flex-start"
-            }}
-        >
-            <Box
-                mt="1"
-                fontWeight="semibold"
-                fontSize="xl"
-                as="h1"
-                lineHeight="tight"
-                noOfLines={1}
-                color={color}
-            >
-                {name}
-            </Box>
-            <Box
-                flexGrow="1"
-                display="flex"
-                flexDirection="column"
-            >
-                <div>
-                    <Box as="span" fontSize="4rem">
-                        {status.pass.length}
-                    </Box>
-                    <Box as="span" fontSize="3rem" m={2}>
-                        /
-                    </Box>
-                    <Box as="span" color="gray.600" fontSize="2rem">
-                        {status.pass.length + status.failed.length}
-                    </Box>
-                    <Box as="span" color="gray.600" m={1} fontSize="1.5rem">
-                        ({status.submitted})
-                    </Box>
-                </div>
-                <Box
-                    maxHeight="50vh"
-                    overflowY="auto"
-                >
-                    {status.submitted !== 0 && groupBy(
-                        UserProblemStatus.fromObject(status).getAll(),
-                        (x: ProblemWithStatus) => `${x.platform}-${x.contest}`
-                    ).sort((a, b) =>
-                    {
-                        const ap = a[0];
-                        const bp = b[0];
-                        return `${ap.platform}-${ap.contest}`.localeCompare(`${bp.platform}-${bp.contest}`);
-                    }).map((group, i) =>
-                    {
-                        return (
-                            <ProblemGroup key={`${name}-${i}`} problems={group} />
-                        );
-                    })}
-                </Box>
-                {status.submitted === 0 && (
-                    <Box fontSize="1.5rem" style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        padding: "1rem",
-                        flexGrow: 1
-                    }}>
-                        <Lottie animationData={animation} />
-                    </Box>
-                )}
-            </Box>
-        </Box>
-    );
+	return (
+		<Box
+			padding={4}
+			key={name}
+			borderWidth="3px"
+			borderRadius="lg"
+			borderColor={color}
+			overflow="hidden"
+			style={{
+				background: `linear-gradient(250deg, ${color} 2rem, var(--chakra-colors-chakra-body-bg) 0)`,
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "flex-start",
+			}}
+		>
+			<Box
+				mt="1"
+				fontWeight="semibold"
+				fontSize="xl"
+				as="h1"
+				lineHeight="tight"
+				noOfLines={1}
+				color={color}
+			>
+				{name}
+			</Box>
+			<Box flexGrow="1" display="flex" flexDirection="column">
+				<div>
+					<Box as="span" fontSize="4rem">
+						{status.pass.length}
+					</Box>
+					<Box as="span" fontSize="3rem" m={2}>
+						/
+					</Box>
+					<Box as="span" color="gray.600" fontSize="2rem">
+						{status.pass.length + status.failed.length}
+					</Box>
+					<Box as="span" color="gray.600" m={1} fontSize="1.5rem">
+						({status.submitted})
+					</Box>
+				</div>
+				{status.submitted !== 0 && (
+					<Box height="50vh" overflowY="auto">
+						{groupBy(
+							UserProblemStatus.fromObject(status).getAll(),
+							(x: ProblemWithStatus) =>
+								`${x.platform}-${x.contest}`,
+						)
+							.sort((a, b) => {
+								const ap = a[0];
+								const bp = b[0];
+								return `${ap.platform}-${ap.contest}`.localeCompare(
+									`${bp.platform}-${bp.contest}`,
+								);
+							})
+							.map((group, i) => {
+								return (
+									<ProblemGroup
+										key={`${name}-${i}`}
+										problems={group}
+									/>
+								);
+							})}
+					</Box>
+				)}
+				{status.submitted === 0 && (
+					<Box
+						fontSize="1.5rem"
+						height="50vh"
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							padding: "1rem",
+							flexGrow: 1,
+						}}
+					>
+						<Lottie animationData={animation} />
+					</Box>
+				)}
+			</Box>
+		</Box>
+	);
 }
 
 function ProblemGroup({
-    problems
+	problems,
 }: {
-    problems: ProblemWithStatus[];
-}): JSX.Element
-{
-    if(problems.length === 0)
-    {
-        return <></>;
-    }
-    problems.sort((a, b) => a.title.localeCompare(b.title));
+	problems: ProblemWithStatus[];
+}): JSX.Element {
+	if (problems.length === 0) {
+		return <></>;
+	}
+	problems.sort((a, b) => a.title.localeCompare(b.title));
 
-    return (
-        <div>
-            <div>
-                <Box
-                    as="span"
-                    color={"gray.500"}
-                    fontSize="0.85rem"
-                >{`${problems[0].platform} ${problems[0].contest}`}</Box>
-            </div>
-            <Box>
-                {problems.map((problem) =>
-                {
-                    return (
-                        <div key={`${problem.id}`}>
-                            <Link href={problem.url} isExternal>
-                                <Box
-                                    as="span"
-                                    className={"codeStyle"}
-                                    color={problem.success ? "green.500" : "red.500"}
-                                    fontSize="1rem"
-                                >
-                                    {problem.title}
-                                </Box>
-                            </Link>
-                        </div>
-                    );
-                })}
-            </Box>
-        </div>
-    );
+	return (
+		<div>
+			<div>
+				<Box
+					as="span"
+					color={"gray.500"}
+					fontSize="0.85rem"
+				>{`${problems[0].platform} ${problems[0].contest}`}</Box>
+			</div>
+			<Box>
+				{problems.map((problem) => {
+					return (
+						<div key={`${problem.id}`}>
+							<Link href={problem.url} isExternal>
+								<Box
+									as="span"
+									className={"codeStyle"}
+									color={
+										problem.success
+											? "green.500"
+											: "red.500"
+									}
+									fontSize="1rem"
+								>
+									{problem.title}
+								</Box>
+							</Link>
+						</div>
+					);
+				})}
+			</Box>
+		</div>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 // noinspection JSIgnoredPromiseFromCall
 
-import { Box, Container, IconButton, SimpleGrid, Stack, Tooltip, useToast } from "@chakra-ui/react";
+import {
+	Box,
+	Container,
+	IconButton,
+	SimpleGrid,
+	Stack,
+	Tooltip,
+	useToast,
+} from "@chakra-ui/react";
 import { PureUserProblemStatus } from "../types/tentacle";
-import React, { useCallback, useMemo, useRef, useState } from "react";
+import React, {
+	useCallback,
+	useEffect,
+	useMemo,
+	useRef,
+	useState,
+} from "react";
 import { UserCard } from "../components/UserCard";
 import { AnimatePresence, motion } from "framer-motion";
 import { GetServerSideProps } from "next";
@@ -13,241 +27,239 @@ import { getNewTimedLogger, readData } from "../utils/utils";
 import { client } from "../constants";
 import NoSSR from "../components/NoSSR";
 
-interface HomeProps{
-    result: Record<string, PureUserProblemStatus>;
+interface HomeProps {
+	result: Record<string, PureUserProblemStatus>;
 }
 
-function Home({
-    result
-}: HomeProps)
-{
-    const [data, setData] = useState(result);
-    const [start, setStart] = useState(0);
+function Home({ result }: HomeProps) {
+	const [data, setData] = useState(result);
+	const [start, setStart] = useState(0);
 
-    const [refreshLoading, setRefreshLoading] = useState(false);
-    const [nextPageLoading, setNextPageLoading] = useState(false);
+	const [refreshLoading, setRefreshLoading] = useState(false);
+	const [nextPageLoading, setNextPageLoading] = useState(false);
 
-    const [animateCnt, setAnimateCnt] = useState(1);
+	const nextTimer = useRef<number | null>(null);
 
-    const enterAnimation = useCallback(() =>
-    {
-        setAnimateCnt((cnt) => cnt + 1);
-        setNextPageLoading(true);
-    }, []);
+	const toast = useToast();
 
-    const exitAnimation = useCallback(() =>
-    {
-        if(animateCnt === 2){
-            setNextPageLoading(false);
-            setAnimateCnt(0);
-        }
-    }, [animateCnt]);
+	const { width } = useWindowSize();
 
-    const toast = useToast();
+	const visibleCardCount = useMemo(() => {
+		return Math.floor(Math.min(width, 1280) / 300);
+	}, [width]);
 
-    const lastClick = useRef<number>(Date.now());
+	const displayCardCount = useMemo(() => {
+		return Math.min(visibleCardCount, Object.keys(data).length);
+	}, [data, visibleCardCount]);
 
-    const { width } = useWindowSize();
+	const updateInterval = useMemo(() => {
+		return 1000 * 5 * displayCardCount;
+	}, [displayCardCount]);
 
-    const visibleCardCount = useMemo(() =>
-    {
-        return Math.floor(Math.min(width, 1280) / 300);
-    }, [width]);
+	const sortedData = useMemo(() => {
+		return Object.entries(data);
+	}, [data]);
 
-    const updateInterval = useMemo(() =>
-    {
-        return 1000 * 5 * visibleCardCount;
-    }, [visibleCardCount]);
+	const cards = useMemo(() => {
+		return sortedData.map(([name, status]) => {
+			return <UserCard key={name} name={name} status={status} />;
+		});
+	}, [sortedData]);
 
-    const sortedData = useMemo(() =>
-    {
-        return Object.entries(data);
-    }, [data]);
+	const visibleCards = useMemo(() => {
+		const _cards = [];
+		for (let i = start; i < start + displayCardCount; i++) {
+			_cards.push(cards[i % cards.length]);
+			if (_cards.length >= cards.length) {
+				break;
+			}
+		}
+		return _cards;
+	}, [start, displayCardCount, cards]);
 
-    const cards = useMemo(() =>
-    {
-        return sortedData.map(([name, status]) =>
-        {
-            return <UserCard key={name} name={name} status={status} />;
-        });
-    }, [sortedData]);
+	const refresh = useCallback(async () => {
+		if (refreshLoading) {
+			return;
+		}
 
-    const visibleCards = useMemo(() =>
-    {
-        const _cards = [];
-        for(let i = start; i < start + visibleCardCount; i++)
-        {
-            _cards.push(cards[i % cards.length]);
-            if(_cards.length >= cards.length)
-            {
-                break;
-            }
-        }
-        return _cards;
-    }, [start, visibleCardCount, cards]);
+		setRefreshLoading(true);
+		const res = await fetch("/api/data");
+		if (res.status === 200) {
+			const data = await res.json();
+			setData(data);
+		}
+		setRefreshLoading(false);
+	}, [refreshLoading]);
 
-    const refresh = useCallback(async () =>
-    {
-        if(refreshLoading)
-        {
-            return;
-        }
+	useInterval(async () => {
+		await refresh();
+	}, updateInterval * 8);
 
-        setRefreshLoading(true);
-        const res = await fetch("/api/data");
-        if(res.status === 200)
-        {
-            const data = await res.json();
-            setData(data);
-        }
-        setRefreshLoading(false);
-    }, [refreshLoading]);
+	const goNext = useCallback(() => {
+		setStart((start + displayCardCount) % cards.length);
+	}, [start, displayCardCount, cards.length]);
 
-    useInterval(async () =>
-    {
-        await refresh();
-    }, updateInterval * 4);
+	const resetNextTimer = useCallback(() => {
+		if (nextTimer.current !== null) {
+			clearTimeout(nextTimer.current);
+		}
+		nextTimer.current = window.setTimeout(() => {
+			goNext();
+		}, updateInterval);
+	}, [goNext, updateInterval]);
 
-    const goNext = useCallback(() =>
-    {
-        setStart((start + visibleCardCount) % cards.length);
-        lastClick.current = Date.now();
-    }, [start, visibleCardCount, cards.length]);
+	useEffect(() => {
+		resetNextTimer();
+	}, [resetNextTimer]);
 
+	const onAnimateEnter = useCallback(() => {
+		setNextPageLoading(true);
+	}, []);
 
-    useInterval(goNext, updateInterval);
+	const onAnimateExit = useCallback(() => {
+		setNextPageLoading(false);
+	}, []);
 
-    return (
-        <NoSSR>
-            <TokenDialog />
-            <Container maxW="container.xl">
-                {visibleCardCount !== 1 && <Box className={"activeCardIndicator"}>
-                    <Stack direction={"column"} spacing={6}>
-                        <UpdateButton />
-                        <Stack direction={"column"} spacing={2}>
-                            <Tooltip label="刷新" placement="right">
-                                <IconButton
-                                    isLoading={refreshLoading}
-                                    aria-label={"refresh"}
-                                    icon={<RepeatIcon />}
-                                    variant={"solid"}
-                                    onClick={() =>
-                                    {
-                                        refresh().then(() => {
-                                            toast({
-                                                title: "刷新成功",
-                                                status: "success",
-                                                duration: 2000,
-                                                isClosable: true,
-                                                position: "top"
-                                            });
-                                        }).catch(() => {
-                                            toast({
-                                                title: "刷新失败",
-                                                status: "error",
-                                                duration: 2000,
-                                                isClosable: true,
-                                                position: "top"
-                                            });
-                                        });
-
-                                    }}
-                                />
-                            </Tooltip>
-                            <Tooltip label="下一页" placement="right">
-                                <IconButton
-                                    isLoading={nextPageLoading}
-                                    aria-label={"next page"}
-                                    icon={<TriangleDownIcon />}
-                                    onClick={goNext}
-                                />
-                            </Tooltip>
-                        </Stack>
-                    </Stack>
-                </Box>}
-                <Box m={6} p={6}>
-                    <AnimatePresence
-                        mode="wait">
-                        <motion.div
-                            key={`cards-${start}`}
-                            initial={{ y: 10, opacity: 0 }}
-                            animate={{ y: 0, opacity: 1 }}
-                            exit={{ y: -10, opacity: 0 }}
-                            transition={{ duration: 1 }}
-                            onAnimationStart={enterAnimation}
-                            onAnimationComplete={exitAnimation}
-                        >
-                            <SimpleGrid columns={Math.min(visibleCardCount, visibleCards.length)} spacing={10}>
-                                {visibleCards}
-                            </SimpleGrid>
-                        </motion.div>
-                    </AnimatePresence>
-                </Box>
-            </Container>
-        </NoSSR>
-    );
+	return (
+		<NoSSR>
+			<TokenDialog />
+			<Container
+				width={"calc(100vw - 2rem)"}
+				height={"100%"}
+				maxW="container.xl">
+				{visibleCardCount !== 1 && (
+					<Box className={"activeCardIndicator"}>
+						<Stack direction={"column"} spacing={6}>
+							<UpdateButton />
+							<Stack direction={"column"} spacing={2}>
+								<Tooltip label="刷新" placement="right">
+									<IconButton
+										isLoading={refreshLoading}
+										aria-label={"refresh"}
+										icon={<RepeatIcon />}
+										variant={"solid"}
+										onClick={() => {
+											refresh()
+												.then(() => {
+													toast({
+														title: "刷新成功",
+														status: "success",
+														duration: 2000,
+														isClosable: true,
+														position: "top",
+													});
+												})
+												.catch(() => {
+													toast({
+														title: "刷新失败",
+														status: "error",
+														duration: 2000,
+														isClosable: true,
+														position: "top",
+													});
+												});
+										}}
+									/>
+								</Tooltip>
+								<Tooltip label="下一页" placement="right">
+									<IconButton
+										isLoading={nextPageLoading}
+										aria-label={"next page"}
+										icon={<TriangleDownIcon />}
+										onClick={() => {
+											goNext();
+											resetNextTimer();
+										}}
+									/>
+								</Tooltip>
+							</Stack>
+						</Stack>
+					</Box>
+				)}
+				<Box m={6} p={6}>
+					<AnimatePresence
+						initial={false}
+						mode="popLayout"
+					>
+						<motion.div
+							key={`cards-${start}`}
+							initial={{ y: 0, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+							exit={{ y: 0, opacity: 0 }}
+							transition={{ duration: 2 }}
+							onAnimationStart={onAnimateEnter}
+							onAnimationComplete={onAnimateExit}
+						>
+							<SimpleGrid columns={displayCardCount} spacing={10}>
+								{visibleCards}
+							</SimpleGrid>
+						</motion.div>
+					</AnimatePresence>
+				</Box>
+			</Container>
+		</NoSSR>
+	);
 }
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async ({ res }) =>
-{
-    res.setHeader("Cache-Control", "public, s-maxage=15, stale-while-revalidate=60");
+export const getServerSideProps: GetServerSideProps<HomeProps> = async ({
+	res,
+}) => {
+	res.setHeader(
+		"Cache-Control",
+		"public, s-maxage=15, stale-while-revalidate=60",
+	);
 
-    const logger = getNewTimedLogger();
-    const data = await readData(client, logger);
-    console.info(logger("getResult"));
-    if(!data)
-    {
-        return {
-            props: {
-                result: {}
-            }
-        };
-    }
-    const result: Record<string, PureUserProblemStatus> = JSON.parse(data);
-    return {
-        props: {
-            result
-        }
-    };
+	const logger = getNewTimedLogger();
+	const data = await readData(client, logger);
+	console.info(logger("getResult"));
+	if (!data) {
+		return {
+			props: {
+				result: {},
+			},
+		};
+	}
+	const result: Record<string, PureUserProblemStatus> = JSON.parse(data);
+	return {
+		props: {
+			result,
+		},
+	};
 };
 
-const UpdateButton: React.FC = () =>
-{
-    const [loading, setLoading] = useState(false);
-    const toast = useToast();
+const UpdateButton: React.FC = () => {
+	const [loading, setLoading] = useState(false);
+	const toast = useToast();
 
-    return <Tooltip label="重新爬取" placement="right">
-        <IconButton
-            aria-label={"update"}
-            icon={<SpinnerIcon />}
-            isLoading={loading}
-            onClick={() =>
-            {
-                if(!loading)
-                {
-                    setLoading(true);
-                    fetch("/api/refresh", {
-                        method: "POST"
-                    })
-                        .then((r) =>
-                        {
-                            setLoading(false);
-                            toast({
-                                title: "更新成功",
-                                status: "success",
-                                duration: 2000,
-                                position: "top"
-                            });
+	return (
+		<Tooltip label="重新爬取" placement="right">
+			<IconButton
+				aria-label={"update"}
+				icon={<SpinnerIcon />}
+				isLoading={loading}
+				onClick={() => {
+					if (!loading) {
+						setLoading(true);
+						fetch("/api/refresh", {
+							method: "POST",
+						}).then((r) => {
+							setLoading(false);
+							toast({
+								title: "更新成功",
+								status: "success",
+								duration: 2000,
+								position: "top",
+							});
 
-                            r.json().then((data) =>
-                            {
-                                console.log(data.log);
-                            });
-                        });
-                }
-            }}
-        />
-    </Tooltip>;
+							r.json().then((data) => {
+								console.log(data.log);
+							});
+						});
+					}
+				}}
+			/>
+		</Tooltip>
+	);
 };
 
 export default Home;
